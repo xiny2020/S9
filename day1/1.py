faf253be76ed3f8374c851a2fc8839f2ca3bf25f
xin y <1064657277@qq.com>
2018-08-19 20:59:05 +0800

    aaa

--- day1/1.py
+++ day1/1.py
@@ -2,5 +2,60 @@
 # -*- coding: utf-8 -*-
 # __author__.=."xin y"
 # .Date:.2018/8/19
-
+'''
 name = input("你叫什么？")
+print(name)
+
+name = input("你叫什么")
+age = input("你多大")
+hometown = input("你从哪里来")
+print("Hello", name , "you are" , age , "years old , you came from" , hometown)
+
+msg = '''
+
+'''
+print(msg)
+
+i=0
+while i < 11 :
+    i = i + 1
+    if i < 7  or i > 7 :
+        print(i)
+        if i > 10:
+            i = 0
+
+i = 0
+sum = 0
+while i<101:
+    sum = i + sum
+    i = i + 1
+print(sum)
+
+i=0
+ji=0
+ou=0
+yu=0
+while i <101 :
+    yu=i%2
+    if yu :
+        ou = ou+i
+        i=i+1
+    else:
+        ji=ji+i
+        i=i+1
+print(ou,ji)
+
+i=1
+h=0
+sum=0
+while i<100:
+    h = i%2
+    if h :
+        sum=sum+i
+        i=i+1
+    else:
+        sum=sum-i
+        i=i+1
+print(sum)
+'''
+#练习
